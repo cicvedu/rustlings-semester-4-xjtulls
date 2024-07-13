fcc3b2f2e2a9d15af932c1efd44d3911686eb562
root <root@47.108.128.143>
2024-07-13 17:38:10 +0800

    fix:macros3

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,17 +5,14 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-mod macros {
-    #[macro_export]
+#[macro_use]
+pub mod macros {
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
 }
-
-use macros::my_macro;
-
 fn main() {
     my_macro!();
 }
